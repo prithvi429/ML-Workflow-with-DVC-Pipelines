8241652216391524cde7a4dcc20278f26dc4e3c1
Pruthviraj Rathod <prithvirathod29884@gmail.com>
2025-07-14 18:33:37 +0530

    updates code

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "ML-Workflow-with-DVC-Pipelines"
+AUTHOR_USER_NAME = "prithvi429"
+SRC_REPO = "chicken_disease_detection"
+AUTHOR_EMAIL = "prithvirathod29884@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="End To End Deep Learning Project Using MLOPS DVC Pipeline for chicken disease detection using DVC.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
--- temp.py
+++ temp.py
@@ -20,7 +20,8 @@ list_of_files = [
     "dvc.yaml",
     "params.yaml",
     "setup.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
+    "templates/index.html"
 ]
 
 for filepath in list_of_files:
