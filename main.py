7311f858d5526fd02b23c04d5a7086cf22dca7c0
Pruthviraj Rathod <prithvirathod29884@gmail.com>
2025-07-17 15:22:37 +0530

    updates code

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from src.chicken_disease_detection import logger
+from src.chicken_disease_detection.pipeline.stage_01_data_ingestion import DataIngestionTraningPipeline
 
-logger.info("welcome to my project")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    pipeline = DataIngestionTraningPipeline()
+    pipeline.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x\n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
