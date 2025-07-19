1bcd547fdb573bac03a04f6c13c4e0e2909f131b
Pruthviraj Rathod <prithvirathod29884@gmail.com>
2025-07-19 17:51:36 +0530

    updats code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.chicken_disease_detection import logger
 from src.chicken_disease_detection.pipeline.stage_01_data_ingestion import DataIngestionTraningPipeline
+from src.chicken_disease_detection.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -8,6 +9,23 @@ try:
     pipeline.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x\n\n")
 except Exception as e:
-    logger.exception(e)
-    raise e
+        logger.exception(e)
+        raise e
 
+
+
+SATGE_NAME = "Prepare Base Model stage"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>> stage {SATGE_NAME} started <<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>> stage {SATGE_NAME} completed <<<<<\n\nx==========x\n\n")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+    
